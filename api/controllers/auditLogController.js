5ad0a3c702a439691fe26b6320da6d58b2cf80d9
Richard Ramos <info@richardramos.me>
2017-10-19 07:38:24 -0400

    Adding truffle deployment script

--- api/controllers/auditLogController.js
+++ api/controllers/auditLogController.js
@@ -1,6 +1,10 @@
 'use strict';
 
 import getWeb3 from '../utils/getWeb3' 
+import AuditLogContract from '../../build/contracts/AuditLog'
+
+const contract = require('truffle-contract');
+const auditLogContract = contract(AuditLogContract);
 
 
 module.exports.createLog = function(req, res){
@@ -15,13 +19,35 @@ module.exports.createLog = function(req, res){
 };
 
 module.exports.dataExists = function(req, res){
+
+	auditLogContract.setProvider(web3.currentProvider);
+
 	var response = { 
 		eid: req.body.eid,
 		hash: "abcdefg",
 		exists: false 
 	};
 
-	
+	auditLogContract.deployed()
+		.then((instance) => {
+			instance.getFile.call(req.body.eid)
+				.then((result) => {
+					if(result){
+						console.log("Success");
+					} else {
+						console.log("Fail");
+					}
+				}).catch((err) => {
+					  console.log("Error");
+				});
+		})
+
+	auditLogContract.deployed().getFile(req.body.eid);	
+
+
+	// getFile(bytes32 eid)
+
+
 
 	// TODO connect to ethereum, get info using eid, data
 
--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+var ProviderEngine = require("web3-provider-engine");
+var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
+var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+
+var engine = new ProviderEngine();
+engine.addProvider(new WalletSubprovider(wallet, {}));
+engine.addProvider(new FiltersSubprovider());
+engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider("https://rinkeby.infura.io/M6ZZKsf1MORH8hXk8JWH")));
+engine.start(); 
+
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    test: {
+      network_id: 4,
+      provider: engine,
+      from: address
     }
   }
 };
+
+
