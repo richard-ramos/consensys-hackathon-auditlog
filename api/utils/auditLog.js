3d22503bf48b13aa8589212a27c13b2196edf617
Dulguun Batmunkh <dulguunbatmunkh@gmail.com>
2017-10-20 10:48:37 +0400

    Update auditLog.js
    - retrieval key
    - updateable (versioning)

--- api/utils/auditLog.js
+++ api/utils/auditLog.js
@@ -1,41 +1,112 @@
+import web3 from '../utils/getWeb3'
+
 var ipfs = require('ipfs')
 var stringify = require('json-stable-stringify');
-var lib = require('merkle-btree');
-//var web3 = require('./getWeb3.js');
+var mBTree = require('merkle-btree');
+
+const ONE_MINUTE = 10 * 1000; //60 * 1000;
 
 // Constructor
 function AuditLog() {
     this.ipfs = new ipfs();
-    this.storage = new lib.IPFSStorage(this.ipfs);
-    this.tree = new lib.MerkleBTree(this.storage);
+    this.storage = new mBTree.IPFSStorage(this.ipfs);
+    this.tree = new mBTree.MerkleBTree(this.storage);
+    this.retrievalKey = this.getHash(new Date());
+    this.address_list = [];
+
+    setInterval(this.batchJob, ONE_MINUTE);
 }
 
 // class methods
-// return promise
-AuditLog.prototype.log = function(externalId, userId, jsonObject) {
-    //var key = web3.hash3(externalId);
-    //key = web3.hash3(key + userId);
-    var key = this.secureKey(externalId, userId);
-    var value = stringify(jsonObject);
+AuditLog.prototype.log = function(userId, externalId, jsonObject) {
+    var prev_address_key = this.getHash(externalId, userId, 'ipfs_address');
+    var version = 0;
+    var prev_ipfs_address = "none";
 
-    return this.tree.put(key, value);
+    var prev = this.tree.get(prev_address_key)
+        .then( value => {
+            if (value != null) {
+                version = value.version;
+                prev_ipfs_address = value.ipfs_address;
+            }
+
+            let data = stringify(jsonObject);
+            let dataHash = this.getHash(data);
+            let retrievalKey = this.retrievalKey;
+            var value = {
+                "prev": prev_ipfs_address,
+                "version": version,
+                "retrievalKey": this.retrievalKey,
+                "data": data,
+                "dataHash": dataHash
+            };
+
+            let key = this.getHash(externalId, userId);
+            this.tree.put(key, value)
+                .then( ipfs_address => {
+                    this.address_list.push(ipfs_address);
+
+                    let prev_value = {
+                        'version': version + 1,
+                        'ipfs_address': ipfs_address
+                    };
+                    this.tree.put(prev_address_key, prev_value);
+
+                    console.log("added to ipfs!");
+                });
+        });
+
+    return;
 };
 
-// returns promise
-AuditLog.prototype.audit = function(externalId, userId, jsonObject) {
-    var key = this.secureKey(externalId, userId);
+AuditLog.prototype.audit = function(userId, externalId, jsonObject) {
+    var key = this.getHash(externalId, userId);
+    var minimizedJson = stringify(jsonObject);
+    var jsonHash = this.getHash(minimizedJson);
 
-    return this.tree.get(key);
+    this.tree.get(key)
+        .then( value => {
+            if (value == null) {
+                console.log("Log doesn't exist");
+                return;
+            }
+
+            console.log("values: " + stringify(value));
+            // Compare by hash of the data
+            if (jsonHash == value.dataHash) {
+                console.log("Hash matches!");
+
+                // TODO: get from ethereum using retrievalKey
+                console.log("retrievalKey:" + value.retrievalKey);
+            } else {
+                console.log("Hash doesn't match!");
+            }
+        })
 };
 
-AuditLog.prototype.secureKey = function(externalId, userId) {
-    // TODO: use web3.sha3
-    /*var key = web3.sha3(externalId);
-    key = web3.sha3(key + userId);
+AuditLog.prototype.getHash = function() {
+    var key = "";
+    for (var i = 0; i < arguments.length; i++) {
+        key = web3.sha3(key + arguments[i]);
+    }
 
-    return key;*/
+    return key;
+}
 
-    return externalId + userId;
+AuditLog.prototype.batchJob = function() {
+    console.log("batch job started!");
+    /*
+    this.tree.put(this.retrievalKey, address_list)
+        .then( ipfs_address => {
+
+            address_list = [];  // reset
+            // TODO: call smart contract with
+            // retKey, ipfs_address
+
+            this.retrievalKey = this.getHash(new Date());
+        });
+*/
+    return;
 }
 
 // export the class
