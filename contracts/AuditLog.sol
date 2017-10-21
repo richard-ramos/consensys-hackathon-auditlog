10047af6b26b9027e3ce333ff2de4f993adf5c8f
Kriys94 <christian.trn@gmail.com>
2017-10-21 08:59:00 +0200

    Update AuditLog with new functions

--- contracts/AuditLog.sol
+++ contracts/AuditLog.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.13;
 
 import "./Owned.sol";
 
-contract AuditLog is Owned {
+contract AuditLog is Owned{
 
     struct ipfsInfo {
-        bytes32 ipfsAddress;
+        bytes32 ipfsAddress1;
+        bytes16 ipfsAddress2;
         uint blockNumber;
     }
 
-    event LogIpfsFile(address owner, 
-                      uint blockNumber, 
-                      bytes32 retrievialKey, 
-                      bytes32 ipfsAddress);
+    event LogIPFSfile(address owner, uint blockNumber, bytes32 retrievialKey, bytes32 ipfsAddress1, bytes16 ipfsAddress2);
 
+    bytes32[] ipfsAddresses1;
+    bytes16[] ipfsAddresses2;
 
     mapping(bytes32 => ipfsInfo) public ipfsFiles;
 
@@ -21,6 +21,14 @@ contract AuditLog is Owned {
         public
     {
         
+    }
+    
+    function countAddresses()
+        public
+        returns(uint count)
+    {
+        require(ipfsAddresses1.length == ipfsAddresses2.length);
+        return ipfsAddresses1.length;
     }
 
     function hashKey(bytes32 retrievialKey)
@@ -31,28 +39,41 @@ contract AuditLog is Owned {
         return keccak256(retrievialKey);
     }
 
-    function getIpfsAddress(bytes32 retrievialKey)
+    function getIpfsInfo(bytes32 retrievialKey)
         public
         constant
-        returns(bytes32 ipfsAddress, uint blockNumber)
+        returns(bytes32 ipfsAddress1, bytes16 ipfsAddress2, uint blockNumber)
     {
-        return (ipfsFiles[hashKey(retrievialKey)].ipfsAddress, ipfsFiles[hashKey(retrievialKey)].blockNumber);
+        return (ipfsFiles[hashKey(retrievialKey)].ipfsAddress1, ipfsFiles[hashKey(retrievialKey)].ipfsAddress2, ipfsFiles[hashKey(retrievialKey)].blockNumber);
+    }
+    
+    function getIpfsAddresses(uint id)
+        public
+        constant
+        returns(bytes32 ipfsAddress1, bytes16 ipfsAddress2)
+    {
+        return (ipfsAddresses1[id], ipfsAddresses2[id]);
     }
 
-    function addFile(bytes32 retrievialKey, bytes32 ipfsAddress)
+    function addFile(bytes32 retrievialKey, bytes32 ipfsAddress1, bytes16 ipfsAddress2)
         public
         fromOwner
         returns(bool success)
     {
         bytes32 hash = hashKey(retrievialKey);
 
-        require(ipfsFiles[hash].ipfsAddress != ipfsAddress);
+        require(ipfsFiles[hash].ipfsAddress1 != ipfsAddress1);
+        require(ipfsFiles[hash].ipfsAddress2 != ipfsAddress2);
         require(ipfsFiles[hash].blockNumber == 0);
 
-        ipfsFiles[hash].ipfsAddress = ipfsAddress;
+        ipfsFiles[hash].ipfsAddress1 = ipfsAddress1;
+        ipfsFiles[hash].ipfsAddress2 = ipfsAddress2;
         ipfsFiles[hash].blockNumber = block.number;
         
-        LogIpfsFile(msg.sender, block.number, retrievialKey, ipfsAddress);
+        ipfsAddresses1.push(ipfsAddress1);
+        ipfsAddresses2.push(ipfsAddress2);
+        
+        LogIPFSfile(msg.sender, block.number, retrievialKey, ipfsAddress1, ipfsAddress2);
         
         return true;
     }
